9b477cea2e7e63099fe42b7c5b7432c16a2aa81e
Nikita Korotkiy <nikita.k@orot-technologies.co.il>
2020-09-15 10:27:02 +0300

    Add cycle and rules

--- script.js
+++ script.js
@@ -2,20 +2,36 @@
 
 const numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-const lastSeenFilm = prompt("Один из последних просмотренных фильмов?", ""),
-      lastFilmRating = prompt("На сколько оцените его?", ""),
-      preLastSeenFilm = prompt("Один из последних просмотренных фильмов?", ""),
-      preLastFilmRating = prompt("На сколько оцените его?", "");
-
 let personalMovieDB = {
     count: numberOfFilms,
-    movies: {
-        [lastSeenFilm]: lastFilmRating, 
-        [preLastSeenFilm]: preLastFilmRating
-    },
+    movies: {},
     actors: {},
     genres: [],
     privat: false
 };
+
+if (personalMovieDB.count < 10) {
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count < 30 && personalMovieDB.count >= 10) {
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
+
+for (let i = 0; i< 2; i++) {
+
+    const lastFilm = prompt("Один из последних просмотренных фильмов?", "");
+    const lastFilmRating = prompt("На сколько оцените его?", "");
+
+    if (lastFilm != "" && lastFilmRating != null && lastFilmRating != "" && lastFilm != null && lastFilmRating != "" && lastFilm.length > 50) {
+        personalMovieDB.movies[lastFilm] = lastFilmRating;
+    } else {
+        i--;
+    }
+
+}
+
 
 console.log(personalMovieDB);
